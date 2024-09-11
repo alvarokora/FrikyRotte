7888ebecb3f530c1a0c530dd06dbb68161505982
Alvaro <alvaromora50@gmail.com>
2024-09-11 19:41:06 -0300

    Errores

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'Test',
-  webDir: 'www'
-};
-
-export default config;
